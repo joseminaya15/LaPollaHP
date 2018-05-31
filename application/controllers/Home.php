d6596bdf7f48c682d73034da76b85ae6ba2ad115
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-05-31 13:20:33 -0500

    cambios

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,8 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	public function index()
-	{
+	public function index(){
+		$datos = file_get_contents('http://www.marketinghp.net/microsite/La_Polla_HP/Controladores/Registros');
+		//echo $datos;
+		//exit;
 		$this->load->view('v_home');
 	}
 }
