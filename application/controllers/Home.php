c47f9883fc2f3eaa788d49d2ca0a8c29ef562744
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-06-04 18:10:42 -0500

    cambios

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends CI_Controller {
 
 	function __construct() {
         parent::__construct();
-        //$this->load->model('M_datos');
+        $this->load->model('M_datos');
         $this->load->helper("url");
         $this->output->set_header('Last-Modified:'.gmdate('D, d M Y H:i:s').'GMT');
         $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate');
@@ -19,8 +19,8 @@ class Home extends CI_Controller {
 		$html1 = '';
 		$json = file_get_contents($url);
 	    $obj  = json_decode($json);*/
-	    $data['nombre']   = isset($_GET['code']) == true ? base64_decode($_GET['code']) : '-';
-	    $canti 			  = isset($_GET['cantidad']) == true ? base64_decode($_GET['cantidad']) : 1;
+	    $data['nombre']   = isset($_GET['nombre']) == true ? base64_decode($_GET['nombre']) : '-';
+	    $canti 			  = isset($_GET['acumulado']) == true ? base64_decode($_GET['acumulado']) : 1;
 	    $data['cantidad'] = $canti ;
 	    $multi = $canti/5000;
 	    $data['puntos']   = '0';
@@ -31,7 +31,12 @@ class Home extends CI_Controller {
 		$data['error'] = EXIT_ERROR;
 		$data['msj']   = null;
 		try {
-
+			$cont = $this->input->post('cont');
+			$cont = $this->input->post('cont');
+			$cont = $this->input->post('cont');
+			// $arrayInsert = array('marcador'   => $datos,
+   //                                'Id_lenguaje' => $idIdioma);
+   //          $datoInsert = $this->M_datos->insertarDatos($arrayInsert, 'anotaciones');
 			$data['error'] = EXIT_SUCCESS;
 		}catch(Exception $e){
 			$data['msj'] = $e->getMessage();
--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,22 @@
+var cont = 1;
 function guardarScore(pais, empate, id){
-	console.log(pais);
-	console.log(empate);
 	$('#'+id).css('background', '#D0D0D0');
   $("#"+id+" input").each(function (){ 
     $('#'+this.id).prop( "disabled", true);
   });
 	$.ajax({
 		data : {pais   : pais,
-				    empate : empate},
+				    empate : empate,
+            cont   : cont},
 		url  : 'Home/guardarScore',
 		type : 'POST'
 	}).done(function(data){
 		try{
         data = JSON.parse(data);
         if(data.error == 0){
-        	$('.tabla').html('');
-        	$('.tabla').append(data.tabla);
+        	/*$('.tabla').html('');
+        	$('.tabla').append(data.tabla);*/
+          cont++;
         }else{
         	return;
         }
